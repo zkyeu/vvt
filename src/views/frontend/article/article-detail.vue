ec447fa4c31d7ecd74ab3597be6455f48ced3cac
liliang <zkyeu@163.com>
2022-02-22 21:41:57 +0800

    feat: localstorage

--- src/views/frontend/article/article-detail.vue
+++ src/views/frontend/article/article-detail.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-08-16 11:59:19
- * @LastEditTime: 2022-01-24 14:23:45
+ * @LastEditTime: 2022-02-22 21:41:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vvt/src/views/frontend/article/article-detail.vue
@@ -13,18 +13,18 @@
       <div class="main-content">
         <Content />
         <div class="basic-info">
-          <div>
+          <div @click="handleClick('shoucang', basicInfo.shoucang)">
             <icon :data="shoucang"></icon>
             <spa v-if="true">收藏</spa>
           </div>
-          <div>
+          <div @click="handleClick('pinglun', basicInfo.pinglun)">
             <icon :data="pinglun"></icon>
             <span v-if="true">{{ commLength }}</span>
             <span v-else>评论</span>
           </div>
-          <div>
-            <icon :data="zan"></icon>
-            <span v-if="true">{{ 9 }}</span>
+          <div @click="handleClick('zan', basicInfo.zan)">
+            <icon :data="basicInfo.zan ? zaned : zan" :class="{ active: basicInfo.zan }"></icon>
+            <span v-if="true">{{ 19 }}</span>
             <span v-else>点赞</span>
           </div>
         </div>
@@ -44,7 +44,7 @@
 </template>
 
 <script lang="ts">
-  import { ref, defineComponent, onMounted } from 'vue';
+  import { ref, defineComponent, onMounted, reactive } from 'vue';
   import { useRoute } from 'vue-router';
   import Content from './content.vue';
   import Comment from '../../../components/front/comment.vue';
@@ -52,8 +52,10 @@
   import Types from '@com/front/types.vue';
   import shoucang from 'assets/svg/origin/shoucang.svg';
   import pinglun from 'assets/svg/origin/pinglun.svg';
-  import zan from 'assets/svg/origin/zan.svg';
+  import zaned from 'assets/svg/origin/praise.svg';
+  import zan from 'assets/svg/origin/praise-o.svg';
   import { testDevice } from '../../../utils/util';
+  import { log } from 'console';
 
   export default defineComponent({
     name: 'Article-detail',
@@ -64,6 +66,11 @@
       const aid = ref();
       const getNew = ref();
       const commLength = ref(0);
+      const basicInfo = reactive({
+        shoucang: 0,
+        pinglun: 0,
+        zan: 0,
+      });
 
       const fetchCommentInfo = () => {
         // console.log('获取评论信息');
@@ -79,23 +86,61 @@
         let dom = document.querySelector('.content-right');
         (dom as any).style.display = 'none';
       };
+      const changeValue = (obj: any, v: any, i: any) => {
+        if (i) {
+          LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), zan: 0 }));
+          basicInfo.zan = 0;
+        } else {
+          LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), zan: 1 }));
+          basicInfo.zan = 1;
+        }
+      };
+      const handleClick = (v: string, i: any) => {
+        let LS = window.localStorage;
+        let localBasicInfo: any = LS.getItem('basicInfo');
+
+        switch (v) {
+          case 'zan':
+            if (i) {
+              LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), zan: 0 }));
+              basicInfo.zan = 0;
+            } else {
+              LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), zan: 1 }));
+              basicInfo.zan = 1;
+            }
+            break;
+          case 'pinglun':
+            LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), pinglun: 1 }));
+            basicInfo.pinglun = 1;
+            break;
+          case 'shoucang':
+            LS.setItem('basicInfo', JSON.stringify({ ...JSON.parse(localBasicInfo), shoucang: 1 }));
+            basicInfo.shoucang = 1;
+            break;
+          default:
+        }
+      };
 
       onMounted(() => {
         aid.value = route.query.id;
         if (testDevice()) {
           displayDom();
         }
+        let LS = window.localStorage;
       });
 
       return {
         shoucang,
         pinglun,
         zan,
+        zaned,
         aid,
         fetchCommentInfo,
         getNew,
         commLengthFn,
         commLength,
+        basicInfo,
+        handleClick,
       };
     },
   });
@@ -138,6 +183,9 @@
       height: 18px;
       color: #333;
       margin-right: 3px;
+      &.active {
+        color: #2483ff;
+      }
     }
     .content-right {
       width: 300px;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-28 15:51:41
- * @LastEditTime: 2022-02-11 12:26:33
+ * @LastEditTime: 2022-02-14 09:46:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vvt/vite.config.ts
@@ -12,8 +12,8 @@ const path = require('path');
 import svgicon from 'vite-plugin-svgicon';
 
 const proxy = {
-//  target: 'http://localhost:3000/'
-  target: 'https://www.sotm.cn/'
+ target: 'http://localhost:3000/'
+  // target: 'https://www.sotm.cn/'
 }
 export default defineConfig({
   plugins: [vue(), svgicon({
